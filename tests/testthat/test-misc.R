6fefdb9908b4709074421944ba51570a693b387e
AbrahamAz <abraham.azar30@outlook.com>
2023-05-19 16:13:35 +0300

    add inequality checker

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -1,4 +1,4 @@
-test_that("equality works", {
+testthat::test_that("equality works", {
 
   testdf <- data.frame(a = c(1,2),
                        b = c(1,3))
@@ -6,12 +6,21 @@ test_that("equality works", {
                                 b = c(1,3),
                                 c = c(TRUE,FALSE))
 
-  expect_equal(testdf %>%
+  testthat::expect_equal(testdf %>%
                  dplyr::mutate(c = a %==% b), expected_output)
 
 
 })
 
 
+testthat::test_that("inequality works", {
+  testdf <- data.frame(a = c(1,2),
+                       b = c(1,3))
+  expected_output <- data.frame(a = c(1,2),
+                                b = c(1,3),
+                                c = c(FALSE,TRUE))
 
+  testthat::expect_equal(testdf %>%
+                           dplyr::mutate(c = a %!=% b), expected_output)
+})
 
