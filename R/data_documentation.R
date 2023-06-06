e3919fced69a6580d0f7424d30f593cc746385bd
AbrahamAz <abraham.azar30@outlook.com>
2023-06-06 15:27:09 +0200

    add documentation for Data, and the %>%

---
+++ R/data_documentation.R
@@ -0,0 +1,38 @@
+############################### data documentation #######################
+
+
+#' Survey data
+#'
+#' These data sets include HH data (raw and clean data).
+#'
+#' @format NULL
+#' @examples
+#' utilityR_raw_data
+#' utilityR_clean_data
+#' utilityR_survey
+#' utilityR_choices
+
+
+#' @name utilityR_raw_data
+#' @title Raw data
+#' @rdname utilityR_raw_data
+#' @format NULL
+"utilityR_raw_data"
+
+#' @name utilityR_clean_data
+#' @title Clean data
+#' @rdname utilityR_clean_data
+#' @format NULL
+"utilityR_clean_data"
+
+#' @name utilityR_survey
+#' @title Survey Kobo tool
+#' @rdname utilityR_survey
+#' @format NULL
+"utilityR_survey"
+
+#' @name utilityR_choices
+#' @title Choices Kobo Tool
+#' @rdname utilityR_choices
+#' @format NULL
+"utilityR_choices"
